60c3593c5d982c841a3f9e529ceef9b85c05e32b
Jorge de Paz <alejandro131093@gmail.com>
2022-10-28 16:44:11 -0600

    Actualizaciones

--- searchH2.py
+++ searchH2.py
@@ -81,14 +81,13 @@ wholePages = [#Actualizado 18/10/2022
 
 ]
 wholeDict = {}
-href2 = list()
+h22 = list()
 for page in wholePages:
-    #para obtener informacion de temas con paginas
-    #urlPagina = 'https://ayuda.baccredomatic.com/es/seguros-y-coberturas?subcategory=Tipos%20de%20Seguro' 
+##--------------------------Código para la pagina de temas, aqui se obtiene la data de cada articulo de ver todos--------------------------------
+    #para obtener informacion de temas con paginas 
     urlPagina = page 
     # Ejecutar GET-Request
     response = requests.get(urlPagina)
-
     # Analizar sintácticamente el archivo HTML de BeautifulSoup del texto fuente
     html = BeautifulSoup(response.text, 'html.parser')
 
@@ -115,8 +114,8 @@ for page in wholePages:
 
 
 
-    #Web scrapping del articulo para setear el flag GT y obtener la última actualización 
-    href = list()
+    #Web scrapping del articulo para buscar todos lo H2 dentro de cada articulo
+    h2 = list()
     flagOPA = list()
 
     testDict ={}
@@ -124,23 +123,34 @@ for page in wholePages:
     for hiperlink in hiperlinkHtmlExtended:
         responseArticle = requests.get(hiperlink)
         htmlArticle = BeautifulSoup(responseArticle.text, 'html.parser')
+        #-------La linea de abajo hace referencia a todo el body-------
         soup = htmlArticle.find('div', {"class" :"row bs-1col node node--type-book node--view-mode-full"}) 
         
-        for a in soup.find_all('a', href=True):
-            href.append(a['href'])
-            href2.append(a['href'])
+        #------La linea siguiente busca todos los anchor, se comentará para dejar el template
+        # for a in soup.find_all('a', href=True):
+        #     href.append(a['href'])
+        #     href2.append(a['href'])
+        
+        #------La linea siguiente buscará todos los h2
+        
+        #all_in = soup.find('h2', {"class" :"content-navigation-0 arrow-up accordion-h2"})
+        all_in =  soup.find_all('h2')
+        print(all_in)
+        for header2 in all_in:
+            h2.append(header2.text.strip())
+            h22.append(header2.text.strip())
 
         testDict.setdefault(articleTitles[cont],{})
         testDict[articleTitles[cont]].setdefault("direccion",hiperlink)
-        testDict[articleTitles[cont]].setdefault("hipervinculos",href)
-        href = list()
+        testDict[articleTitles[cont]].setdefault("headers 2",h2)
+        h2 = list()
         cont+=1
     
     wholeDict.update(testDict)
     testDict ={}
     
 json_data = json.dumps(wholeDict,ensure_ascii=False,indent=3).encode('utf8')
-#print(json_data.decode()) 
+print(json_data.decode()) 
 with open('article_h2.json', 'w') as f:
     json.dump(wholeDict, f, indent=2)
     print("Archivo json creado")
@@ -150,9 +160,11 @@ with open('article_h2.json', 'w') as f:
 
 with open('h2.csv', 'w', encoding='UTF8') as f:
     writer = csv.writer(f)
-    writer.writerow(href2)
+    writer.writerow(h22)
     print("Archivo csv creado")
 
 print("Cantidad de articulos analizados: "+str(len(wholeDict)))     
-print("Cantidad total de hiperviculos encontrados: "+str(len(href2)))     
+print("Cantidad total de h2 encontrados: "+str(len(h22)))    
+
+#print("Todos los H2 del último articulo: "+str(all_in))
 #2143
